f998e648f3b7e865d392e8c68933d8bbdcab4c39
daichi <bumpk1002@gmail.com>
2019-11-10 17:58:01 +0900

    [update-responsive]

--- js/index.js
+++ js/index.js
@@ -1,28 +1,35 @@
-new Vue({
-    el: "#app",
-    data: {
-      position: 0
-    },
-    methods:{
-      toTop: function(){
-        //やむなくjQuery
-        $("html,body").animate({scrollTop:0},"slow");
-      }
-    },
-    ready: function(){
-      var self = this;
-      document.onscroll = function(e){
-        self.position = document.documentElement.scrollTop || document.body.scrollTop;
-      }
-    }
-  });
-
-  $(function(){
-    $('#b a').click(function(){
+$(function(){
+    
+    $('#scroll a').click(function(){
         var id =$(this).attr('href');
         var position = $(id).offset().top - 100;
         $('html,body').animate({
           'scrollTop':position
         }, 1500)
     });
-  });
+
+    var topBtn = $('#top-btn');
+    //ボタンを非表示にする
+    topBtn.hide();
+    //スクロールしてページトップから100に達したらボタンを表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+　　　　　　　//フェードインで表示
+            topBtn.fadeIn();
+        } else {
+　　　　　　　//フェードアウトで非表示
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップへ戻る
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+        return false;
+    });
+});
+
+$(document).ready(function() {
+    $('.drawer').drawer();
+});
